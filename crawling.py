f11e1832ca4d6f325baa687f0832cae390ee7757
ed-kyu <seungkyukim.dev@gmail.com>
2022-10-21 23:36:15 +0900

    fix: Transpose dataframe

--- crawling.py
+++ crawling.py
@@ -25,7 +25,7 @@ def crawling_seoul_climate(api_key:str, start_day:str, end_day:str) -> pd.DataFr
         url = f'http://openAPI.seoul.go.kr:8088/{api_key}/json/DailyAverageCityAir/1/25/{day}'
         response = requests.get(url)
         response_json = response.json()
-        
+
         if response.status_code == 200 and 'DailyAverageCityAir' in response_json:
 
             fine_dust_api_result_data_dict[day] = response_json['DailyAverageCityAir']['row']
@@ -65,7 +65,7 @@ def save_climate_data_to_csv(start_day, last_day):
     start = time.time()
     API_KEY = get_apikey("SEOUL_CLIMATE_API_KEY", json_filename="secret.json")
     fine_dust_data_df = crawling_seoul_climate(API_KEY, start_day, last_day)
-    # fine_dust_data_df = fine_dust_data_df.T
+    fine_dust_data_df = fine_dust_data_df.T
     fine_dust_data_df.to_csv('./result.csv', sep=',', na_rep='NaN')
     print("실행 시간 :", time.time() - start)
 
